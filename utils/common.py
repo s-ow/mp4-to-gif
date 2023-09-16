fe5fc8e63572dbf9bcce5004c41aabc7411e1d87
s-ow <107432480+s-ow@users.noreply.github.com>
2023-09-16 11:24:56 +0200

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -14,13 +14,21 @@ accueil = """
                                                    
 
 """
-v = "1.0.0"
+v = "1.1.0"
 dev = "s-ow"
+git = "https://github.com/s-ow/mp4-to-gif"
 
 # COULEURS
 
+w = Fore.WHITE
+b = Fore.BLACK
+g = Fore.LIGHTGREEN_EX
+y = Fore.LIGHTYELLOW_EX
+m = Fore.LIGHTMAGENTA_EX
+c = Fore.LIGHTCYAN_EX
+lr = Fore.LIGHTRED_EX
+lb = Fore.LIGHTBLUE_EX
 r = Fore.RESET
-m = Fore.MAGENTA
 
 # FONCTIONS ===========================================================================
 
@@ -31,5 +39,14 @@ def menu():
     Write.Print("                               __  __ ____  _  _     _           ____ ___ _____ \n", Colors.purple_to_blue, interval=0.000)
     Write.Print("                              |  \/  |  _ \| || |   | |_ ___    / ___|_ _|  ___|\n", Colors.purple_to_blue, interval=0.000)
     Write.Print("                              | |\/| | |_) | || |_  | __/ _ \  | |  _ | || |_   \n", Colors.purple_to_blue, interval=0.000)
-    Write.Print(f"[Dev]:{dev}                    | |  | |  __/|__   _| | || (_) | | |_| || ||  _|  \n", Colors.purple_to_blue, interval=0.000)
-    Write.Print(f"[Version]:{v}               |_|  |_|_|      |_|    \__\___/   \____|___|_|    \n", Colors.purple_to_blue, interval=0.000)
+    Write.Print("                              | |  | |  __/|__   _| | || (_) | | |_| || ||  _|  \n", Colors.purple_to_blue, interval=0.000)
+    Write.Print("                              |_|  |_|_|      |_|    \__\___/   \____|___|_|    \n", Colors.purple_to_blue, interval=0.000)
+    Write.Print(f"[Dev]:{dev}                                                                     \n", Colors.purple_to_blue, interval=0.000)
+    Write.Print(f"[Version]:{v}                                                                   \n", Colors.purple_to_blue, interval=0.000)
+    Write.Print(f"[Github]:{git}                                                                   \n", Colors.purple_to_blue, interval=0.000)
+    print(f"""
+
+{m}[{r}1{m}]{r} Sélectionner un fichier via un chemin d'accès
+{m}[{r}2{m}]{r} Sélectionner un fichier via l'explorateur de fichiers
+{m}[{r}3{m}]{r} {Fore.RED}QUITTER{r}
+""")
