adfd00798c956eb716e55af6033c01eec5a9db35
s-ow <107432480+s-ow@users.noreply.github.com>
2023-09-16 11:24:28 +0200

    Update librairies.py

--- utils/librairies.py
+++ utils/librairies.py
@@ -1,32 +1,11 @@
 import os
 import sys
 import time
+import signal
 import imageio
 import colorama
+import progressbar
+from tkinter import Tk
 from colorama import Fore
+from tkinter.filedialog import askopenfilename
 from pystyle import Add, Anime, Center, Colorate, Colors, System, Write
-
-try:
-    import time
-except:
-    os.system("pip install time")
-    import time
-
-try:
-    import imageio
-except:
-    os.system("pip install imageio")
-    os.system("pip install imageio[ffmpeg]")
-    import imageio
-
-try:
-    import colorama
-except:
-    os.system("pip install colorama")
-    import colorama
-
-try:
-    import pystyle
-except:
-    os.system("pip install pystyle")
-    import pystyle
